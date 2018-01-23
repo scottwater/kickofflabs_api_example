207a9220c61a9aea09a40d0a395d05aab8b57ac7
Scott Watermasysk <scottwater@gmail.com>
2018-01-23 11:08:30 -0500

    more standard Procfile/puma setup

--- config.ru
+++ config.ru
@@ -1,3 +1,8 @@
+require 'rubygems'
+require 'bundler'
+Bundler.setup# :default, (ENV['RACK_ENV'] || 'development')
+
+
 require './app'
 
 run Sinatra::Application
---
+++ puma.rb
@@ -0,0 +1,8 @@
+workers ENV.fetch('WEB_CONCURRENCY') { 2 }.to_i
+threads_count = ENV.fetch('MAX_THREADS') { 5 }.to_i
+threads threads_count, threads_count
+
+rackup DefaultRackup
+port ENV.fetch('PORT') { 3000 }
+environment ENV.fetch('RACK_ENV') { 'development' }
+preload_app!
